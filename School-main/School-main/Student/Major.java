872f6549610f51b69d562b3d25127a7d9166f6d4
acatchpole7 <alex.catchpole@my.simpson.edu>
2025-04-10 12:23:14 -0500

    Delete School-main directory

--- School-main/School-main/Student/Major.java
+++
@@ -1,6 +0,0 @@
-package week3.Student;
-import java.util.ArrayList;
-
-public interface Major {
-    boolean qualifyForMajor(ArrayList<Course> list);
-}
--- School-main/Student/CMSC.java
+++
@@ -1,9 +0,0 @@
-//package week3.Student;
-//import java.util.ArrayList;
-//
-//public class CMSC {
-//
-//    public boolean qualifyForMajor (ArrayList<Course> list) {
-//
-//    }
-//}
